83f937ac7e2f5dd72484148b48c52b4f50252e01
Ivan Biriuk <birukivan1@gmail.com>
2024-02-22 15:40:02 +0200

    update request

--- src/storefront/graphql/send-request.js
+++ src/storefront/graphql/send-request.js
@@ -28,7 +28,9 @@ export const addToCart = async (productId) => {
       const response = await storefront.request({
         query: queries.addToCart,
         variables: {
-          cartId: cartToken,
+          cartId: cartToken.includes("gid")
+            ? cartToken
+            : `gid://shopify/Cart/${cartToken}`,
           merchandiseId: productId,
           quantity: 1,
         },
